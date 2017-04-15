30e8fdf5f8ba7a11767ed3f727716b3c6b428a24
Christopher Jacob Wood <jacob@Christophers-MacBook-Pro.local>
2017-04-14 20:57:13 -0600

    Created Jet class with constructors and getters/setters

---
+++ src/jetsproject/Jet.java
@@ -0,0 +1,53 @@
+package jetsproject;
+
+public class Jet {
+	String aircraftModel;
+	float Speed;
+	int range;
+	double price;
+
+	public Jet() {
+		super();
+	}
+
+	public Jet(String aircraftModel, float speed, int range, double price) {
+		super();
+		this.aircraftModel = aircraftModel;
+		Speed = speed;
+		this.range = range;
+		this.price = price;
+	}
+
+	public String getAircraftModel() {
+		return aircraftModel;
+	}
+
+	public void setAircraftModel(String aircraftModel) {
+		this.aircraftModel = aircraftModel;
+	}
+
+	public float getSpeed() {
+		return Speed;
+	}
+
+	public void setSpeed(float speed) {
+		Speed = speed;
+	}
+
+	public int getRange() {
+		return range;
+	}
+
+	public void setRange(int range) {
+		this.range = range;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+}
